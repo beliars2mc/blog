8b164b62f7d1f10390f7631dd310de21719f5bed
Dmitriy Melnichenko <tairezzzz@gmail.com>
2019-02-06 11:37:28 +0200

    update logo link and content

--- versions/2.12.0/content/themes/casper/assets/built/search.js
+++ versions/2.12.0/content/themes/casper/assets/built/search.js
@@ -1,2 +1,2 @@
-var searchResults=$("#search-results");$("#search-field").ghostHunter({results:"#search-results",before:function(){searchResults.empty()},onKeyUp:!0,info_template:"",result_template:"<h2><a class='search-results-title' href='{{link}}'>{{title}}</a></h2><p class='search-results-date'>{{pubDate}}</p>"});
+var searchField=$("#search-field"),searchResults=$("#search-results");searchField.val(""),searchField.ghostHunter({results:"#search-results",before:function(){searchResults.empty()},onKeyUp:!0,info_template:"",result_template:"<h2><a class='search-results-title' href='/blog{{link}}'>{{title}}</a></h2><p class='search-results-date'>{{pubDate}}</p>"});
 //# sourceMappingURL=search.js.map
--- versions/2.12.0/content/themes/casper/assets/js/search.js
+++ versions/2.12.0/content/themes/casper/assets/js/search.js
@@ -1,13 +1,13 @@
-const searchField = $("#search-field");
-const searchResults = $("#search-results");
+var searchField = $("#search-field");
+var searchResults = $("#search-results");
 
 searchField.val('');
 searchField.ghostHunter({
   results: "#search-results",
-  before: () => searchResults.empty(),
+  before: function() {
+    searchResults.empty()
+  },
   onKeyUp: true,
   info_template: "",
   result_template: "<h2><a class='search-results-title' href='/blog{{link}}'>{{title}}</a></h2><p class='search-results-date'>{{pubDate}}</p>"
 });
-
-
